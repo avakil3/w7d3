2ad2b9ed88fdfd498fdab03dc0a4655c70286405
Shawn-Fries <samfoley275@gmail.com>
2022-01-19 14:17:24 -0500

    started specs for user

--- W7D3/goal_app/spec/factories/users.rb
+++ W7D3/goal_app/spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
-    
+    username {Faker::FunnyName.name}
+    email {Faker::Internet.email}
+    password {Faker::Cannabis.strain}
   end
 end
--- W7D3/goal_app/spec/models/user_spec.rb
+++ W7D3/goal_app/spec/models/user_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should validate_presence_of(:username)}
+
+  it {should validate_presence_of(:password_digest)}
+
+  it {should validate_length_of(:password).is_at_least(6)}
+
+  describe "uniqueness" do
+    before(:each) do
+      create(:user)
+    end
+    it {should validate_uniqueness_of(:username)}
+    it {should validate_uniqueness_of(:session_token)}
+  end
+
+  describe "check password?" do
+    let!(:user) {create(:user)}
+    context "with a valid password" do
+      it "should return true" do
+        expect user.check_password?('password').to_be true
+      end
+    end
+
+    context "with invalid password" do
+      it "should return false" do
+        expect user.check_password?('password123').to_be false
+      end
+    end
+  end
 end
